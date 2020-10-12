77bfabb010733cef8dc030ed7bcf0c420d8803bc
RenanFelice <felice_owns@hotmail.com>
2020-10-12 16:37:34 -0300

    adicionada responsividade font-size cardbody

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,11 @@ import './Card.css'
 
 
 function Card({customStyle, imgPath,imgStyle, text}) {
-
+    let bg = require(`../Images/img-${imgPath}.jpg`)
     return (
         <div className={`card ${customStyle}`}>
             <div className='cardImgContainer'>
-                <img className='cardImg' 
-                style={imgStyle}
-                src={require(`../Images/img-${imgPath}.jpg`)} alt=""/>
+                <div style={{backgroundImage: `url(${bg})` }} className='img'></div>
             </div>
             <div className='cardBody'>
                 <p className='cardDesc'>
